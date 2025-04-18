c8b080ec9c2f0d0f3e3ca77ba78c688d38ed2210
maton369 <ysk901085@gmail.com>
2025-04-18 11:58:39 +0900

    [Add] フロントのファイルを修正

---
+++ app/user/register/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useState, FormEvent } from "react";
+
+const Register = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/user/register", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      const jsonData: { message: string } = await response.json();
+      alert(jsonData.message);
+    } catch {
+      alert("ユーザー登録失敗");
+    }
+  };
+
+  return (
+    <div>
+      <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          name="name"
+          placeholder="名前"
+          required
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
+        <button type="submit">登録</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
